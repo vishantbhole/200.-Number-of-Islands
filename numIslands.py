35987e934f1b179c48ba0f2d86e47aec97418e37
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-10-01 22:34:20 -0500

    Update numIslands.py

--- numIslands.py
+++ numIslands.py
@@ -4,3 +4,10 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+
+        visited = set()
+        islands = 0
