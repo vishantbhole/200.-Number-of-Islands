a5eb8633e0bb1dd56f68f1962d8aacf28b4046fb
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-10-01 22:36:12 -0500

    Update numIslands.py

--- numIslands.py
+++ numIslands.py
@@ -35,3 +35,15 @@ class Solution:
                     bfs(r,c)
                     islands += 1
         return islands
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    grid = [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+    ]
+
+    print("Output is : ", sol.numIslands(grid))
